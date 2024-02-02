131f4f9a897cfb66db88fbdb560eac84b305f463
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-02 15:46:31 -0600

    Skaffold blocky API

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	appsv1alpha1 "github.com/UnstoppableMango/blocky-controller/api/v1alpha1"
+	"github.com/UnstoppableMango/blocky-controller/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -42,6 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(appsv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.BlockyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("blocky-controller"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Blocky")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
---
+++ internal/controller/blocky_controller_test.go
@@ -0,0 +1,154 @@
+/*
+Copyright 2024 UnstoppableMango.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	//nolint:golint
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	appsv1alpha1 "github.com/UnstoppableMango/blocky-controller/api/v1alpha1"
+)
+
+var _ = Describe("Blocky controller", func() {
+	Context("Blocky controller test", func() {
+
+		const BlockyName = "test-blocky"
+
+		ctx := context.Background()
+
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      BlockyName,
+				Namespace: BlockyName,
+			},
+		}
+
+		typeNamespaceName := types.NamespacedName{
+			Name:      BlockyName,
+			Namespace: BlockyName,
+		}
+		blocky := &appsv1alpha1.Blocky{}
+
+		BeforeEach(func() {
+			By("Creating the Namespace to perform the tests")
+			err := k8sClient.Create(ctx, namespace)
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("Setting the Image ENV VAR which stores the Operand image")
+			err = os.Setenv("BLOCKY_IMAGE", "example.com/image:test")
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("creating the custom resource for the Kind Blocky")
+			err = k8sClient.Get(ctx, typeNamespaceName, blocky)
+			if err != nil && errors.IsNotFound(err) {
+				// Let's mock our custom resource at the same way that we would
+				// apply on the cluster the manifest under config/samples
+				blocky := &appsv1alpha1.Blocky{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      BlockyName,
+						Namespace: namespace.Name,
+					},
+					Spec: appsv1alpha1.BlockySpec{
+						Size:          1,
+						ContainerPort: 53,
+					},
+				}
+
+				err = k8sClient.Create(ctx, blocky)
+				Expect(err).To(Not(HaveOccurred()))
+			}
+		})
+
+		AfterEach(func() {
+			By("removing the custom resource for the Kind Blocky")
+			found := &appsv1alpha1.Blocky{}
+			err := k8sClient.Get(ctx, typeNamespaceName, found)
+			Expect(err).To(Not(HaveOccurred()))
+
+			Eventually(func() error {
+				return k8sClient.Delete(context.TODO(), found)
+			}, 2*time.Minute, time.Second).Should(Succeed())
+
+			// TODO(user): Attention if you improve this code by adding other context test you MUST
+			// be aware of the current delete namespace limitations.
+			// More info: https://book.kubebuilder.io/reference/envtest.html#testing-considerations
+			By("Deleting the Namespace to perform the tests")
+			_ = k8sClient.Delete(ctx, namespace)
+
+			By("Removing the Image ENV VAR which stores the Operand image")
+			_ = os.Unsetenv("BLOCKY_IMAGE")
+		})
+
+		It("should successfully reconcile a custom resource for Blocky", func() {
+			By("Checking if the custom resource was successfully created")
+			Eventually(func() error {
+				found := &appsv1alpha1.Blocky{}
+				return k8sClient.Get(ctx, typeNamespaceName, found)
+			}, time.Minute, time.Second).Should(Succeed())
+
+			By("Reconciling the custom resource created")
+			blockyReconciler := &BlockyReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := blockyReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespaceName,
+			})
+			Expect(err).To(Not(HaveOccurred()))
+
+			By("Checking if Deployment was successfully created in the reconciliation")
+			Eventually(func() error {
+				found := &appsv1.Deployment{}
+				return k8sClient.Get(ctx, typeNamespaceName, found)
+			}, time.Minute, time.Second).Should(Succeed())
+
+			By("Checking the latest Status Condition added to the Blocky instance")
+			Eventually(func() error {
+				if blocky.Status.Conditions != nil &&
+					len(blocky.Status.Conditions) != 0 {
+					latestStatusCondition := blocky.Status.Conditions[len(blocky.Status.Conditions)-1]
+					expectedLatestStatusCondition := metav1.Condition{
+						Type:   typeAvailableBlocky,
+						Status: metav1.ConditionTrue,
+						Reason: "Reconciling",
+						Message: fmt.Sprintf(
+							"Deployment for custom resource (%s) with %d replicas created successfully",
+							blocky.Name,
+							blocky.Spec.Size),
+					}
+					if latestStatusCondition != expectedLatestStatusCondition {
+						return fmt.Errorf("The latest status condition added to the Blocky instance is not as expected")
+					}
+				}
+				return nil
+			}, time.Minute, time.Second).Should(Succeed())
+		})
+	})
+})
